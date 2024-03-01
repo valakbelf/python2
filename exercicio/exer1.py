da7d1f252d0a8dd5a639b8b965d75e725e09a3e0
valakbelf <161715420+valakbelf@users.noreply.github.com>
2024-03-01 09:22:21 -0300

    Add files via upload

---
+++ exercicio/exer1.py
@@ -0,0 +1,23 @@
+idade=25
+nome='yuri'
+altura=1.70
+pessoa=["yuri","25","1.70"]
+ano_nascimento=2006
+salario=40000
+temperatura=("15ºc","35ºc","18ºc")
+aprovado = True
+reprovado = False
+horas_trabalhadas=40
+nota1 = 75
+
+print(idade)
+print(nome)
+print(altura)
+print(pessoa)
+print(ano_nascimento)
+print(salario)
+print(temperatura)
+print(aprovado)
+print(reprovado)
+print(horas_trabalhadas)
+print(nota1)

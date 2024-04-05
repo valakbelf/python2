65379cbc61fcc24b357ddc79ba76e8a25e736f61
valakbelf <161715420+valakbelf@users.noreply.github.com>
2024-04-05 11:28:15 -0300

    Update exer6.py

--- primeiro_tri/lista_prova/exer6.py
+++ primeiro_tri/lista_prova/exer6.py
@@ -1,1 +1,5 @@
-
+numero1 = int(input("digite o primeiro número: "))
+numero2 = int(input("digite o segundo número: "))
+numero3 = int(input("digite o terceiro número: "))
+soma = numero1 + numero2
+print(soma)

85b18b1fa79bbb529c1a35dd221fb31b2eb9e862
valakbelf <161715420+valakbelf@users.noreply.github.com>
2024-04-05 11:27:01 -0300

    Update exer4.py

--- primeiro_tri/lista_prova/exer4.py
+++ primeiro_tri/lista_prova/exer4.py
@@ -1,1 +1,13 @@
+lado1 = float(input("digite a medida do primeiro lado: "))
+lado2 = float(input("digite a medida do segundo lado: "))
+lado3 = float(input("digite a medida do terceiro lado: "))
 
+if lado1==0 or lado2==0 or lado3==0:
+    print("suas medidas não correspondem com um triângulo.")
+else:
+    if lado1==lado2 and lado1==lado3
+    print("seu triângulo é do tipo equilátero.")
+elif lado1!=lado2 and lado1==lado3 or lado1==lado2 and lado1!=lado3or lado2==lado1 and lado2!=lado3 or lado2!=lado1 and lado2==lado3:
+    print("seu triângulo é do tipo isósceles.")
+    else:
+        print("seu triângulo é do tipo escaleno.")

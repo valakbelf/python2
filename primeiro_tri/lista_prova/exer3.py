9b5131321d3c83c098eeea2771a40e7e7658eddb
valakbelf <161715420+valakbelf@users.noreply.github.com>
2024-04-05 11:26:02 -0300

    Update exer3.py

--- primeiro_tri/lista_prova/exer3.py
+++ primeiro_tri/lista_prova/exer3.py
@@ -1,1 +1,9 @@
+        
+nota1 = int(input("digite a nota do 1° bimestre; "))
+nota2 = int(input("digite a nota do 2° bismestre: "))
+nota3 = int(input("digite a nota do 3° bimestre: "))
+nota4 = int(input("digite a nota do 4° bimestre: "))
+
+media = (nota1+nota2+nota3+nota4)//4
+print("sua media foi ", media)
 

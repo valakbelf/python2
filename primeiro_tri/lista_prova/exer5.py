c6e8be67f8826926a8a6fdc86094d5408ff8a3a6
valakbelf <161715420+valakbelf@users.noreply.github.com>
2024-04-05 11:27:40 -0300

    Update exer5.py

--- primeiro_tri/lista_prova/exer5.py
+++ primeiro_tri/lista_prova/exer5.py
@@ -1,1 +1,9 @@
-
+def sum(a, b, c):
+    return (a + b + c)
+    
+    a = int(input("digite o primeiro número: "))
+    b = int(input("digite o segundo número: "))
+    c = int(input("digite o terceiro número:"))
+    
+    print(f"a soma de {a} e {b} e {c} é igual a {sum(a, b, c)}")
+    

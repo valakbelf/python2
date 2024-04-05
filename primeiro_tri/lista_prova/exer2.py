5dea05ce32ff5a550ee3915e19f2b647871ab5fe
valakbelf <161715420+valakbelf@users.noreply.github.com>
2024-04-05 11:25:39 -0300

    Update exer2.py

--- primeiro_tri/lista_prova/exer2.py
+++ primeiro_tri/lista_prova/exer2.py
@@ -1,9 +1,13 @@
-X = int(input("digite um número de dois digitos (menor que 100: " ))
-if x>=100:
-    print("o número é maior que 100")
-    else:
-        dezena= x/100
-        unidade= x%100
-        soma = dezena + unidade 
-        print(soma)
+
         
+numero1 = int(input("digite o primeiro número: "))
+numero2 = int(input("digite o  segundo número: "))
+numero3 = (int(input("digite o terceiro número: "))
+
+if numero1>numero2 and numero1>numero3:
+    print("o maior número é o ",numero1)
+    
+    elif numero2>numero1 and numero2>numero3:
+        print("o maior número é o ",numero2)
+    else:
+        print("o maior número é o ",numero3)
